cac46a17ec00d7b3051a1bc265e2a1bf5acdd3fa
hat021048 <73167096+hat021048@users.noreply.github.com>
2020-12-03 23:53:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,22 +22,27 @@ function Fram () {
     SpeedM2
     )
 }
+function Justering () {
+    SpeedM2 = SpeedM1 * (90 / 100) - 10
+}
 let SpeedM2 = 0
 let SpeedM1 = 0
 basic.showIcon(IconNames.Yes)
 SpeedM1 = 150
-SpeedM2 = SpeedM1 * (100 / 100) - 30
+SpeedM2 = 125
 basic.forever(function () {
     Fram()
     basic.pause(2000)
     Stopp()
     basic.pause(500)
     SpeedM1 = 100
+    Justering()
     Bak()
     basic.pause(2000)
     Stopp()
     basic.pause(500)
     SpeedM1 = 200
+    Justering()
     Fram()
     basic.pause(2000)
     Stopp()
